b6b65f3bb2830e3a17221d4ac7603a5d410780db
Chase Pierce <syntaqx@gmail.com>
2019-08-12 14:51:39 -0600

    use temporary directory for local_file output

--- autotls.tf
+++ autotls.tf
@@ -8,12 +8,17 @@ resource "digitalocean_ssh_key" "swarm" {
   public_key = tls_private_key.generated.public_key_openssh
 }
 
+resource "random_string" "prefix" {
+  length  = 16
+  special = false
+}
+
 resource "local_file" "swarm_private_key" {
-  filename          = format("%s/output/.ssh/%s", path.module, var.cluster_name)
+  filename          = format("/tmp/%s-%s", path.module, random_string.prefix.result, var.cluster_name)
   sensitive_content = tls_private_key.generated.private_key_pem
 }
 
 resource "local_file" "swarm_public_key" {
-  filename          = format("%s/output/.ssh/%s.pub", path.module, var.cluster_name)
+  filename          = format("/tmp/%s-%s.pub", path.module, random_string.prefix.result, var.cluster_name)
   sensitive_content = tls_private_key.generated.public_key_openssh
 }
